ea19ca8ce114790d233fc6cb6632ce3e9d1a08b6
Mojtaba <mojtabaparvizi19@gmail.com>
2024-09-06 12:16:46 -0400

    remodel pop up

--- src/map/PopUp.tsx
+++ src/map/PopUp.tsx
@@ -1,15 +1,21 @@
 import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import { Image, Box, Text, HStack } from "@chakra-ui/react";
 import { CardProp } from "../components/HomeCard";
+import popUp from "../assets/free-location-icon-2955-thumb.png";
 
 interface Prop {
   city: CardProp;
 }
 
 function PopUp({ city }: Prop) {
+  const icon = new L.Icon({
+    iconUrl: popUp,
+    iconSize: [35, 35],
+  });
   return (
     <div>
-      <Marker position={[city.latitude, city.longitude]}>
+      <Marker icon={icon} position={[city.latitude, city.longitude]}>
         <Popup>
           <HStack width={"400px"}>
             <Box>
